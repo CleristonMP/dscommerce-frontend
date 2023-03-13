975f33b1ac690837981b105e131a0564d8e01ef2
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2023-03-13 17:53:00 -0300

    Links

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,8 @@ export default function App() {
         <Route path="/" element={<ClientHome />}>
           <Route index element={<Catalog />} />
           <Route path="catalog" element={<Catalog />} />
-          <Route path="product-details" element={<ProductDetails />} />
+          <Route path="product-details/:productId" element={<ProductDetails />}>
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
