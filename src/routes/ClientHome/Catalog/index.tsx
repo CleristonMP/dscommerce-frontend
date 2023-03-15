49d4047d204dcdb1fde40bab5e14803ea8677d74
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2023-03-15 09:36:00 -0300

    Searchbar onsearch event

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -10,16 +10,23 @@ import "./styles.css";
 export default function Catalog() {
   const [products, setProducts] = useState<ProductDTO[]>([]);
 
+  const [productName, setProductName] = useState("");
+
   useEffect(() => {
-    productService.findAll().then((response) => {
-      setProducts(response.data.content);
-    });
-  }, []);
+    productService.findPageRequest(0, productName)
+      .then((response) => {
+        setProducts(response.data.content);
+      });
+  }, [productName]);
+
+  function handleSearch(searchText: string) {
+    setProductName(searchText);
+  }
 
   return (
     <main>
       <section id="catalog-section" className="dsc-container">
-        <SearchBar />
+        <SearchBar onSearch={handleSearch} />
         <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
           {products.map((product) => (
             <CatalogCard key={product.id} product={product} />
